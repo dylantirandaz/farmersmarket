b4a49f965b99afb0ccd6e28a430a1f080bc95541
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-12-12 11:27:36 -0600

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <MarketList />
+  </div>
+</template>
+
+<script>
+import MarketList from "./MarketList.vue";
+
+export default {
+  components: {
+    MarketList,
+  },
+};
+</script>
