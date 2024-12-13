2acf714277051afd61c5ef169ff04085ea98654d
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-12-13 13:27:54 -0600

    Update farmersmarket.java

--- src/main/java/com/marketfinder/farmersmarket.java
+++ src/main/java/com/marketfinder/farmersmarket.java
@@ -1,16 +1,44 @@
-@Entity
-public class FarmersMarket {
+package com.marketfinder.model;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "farmers_markets")
+public class FarmersMarket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
+    
+    @NotBlank
     private String location;
+    
+    @NotBlank
     private String hours;
+    
     private String dayOfWeek;
-    private String vendorTypes;
+    
+    @ElementCollection
+    @CollectionTable(name = "market_vendor_types")
+    private Set<String> vendorTypes;
+    
     private boolean specialEvent;
-
-//i need to add get and set methods... <construction>
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    private double latitude;
+    private double longitude;
 }
