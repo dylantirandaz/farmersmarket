c722f34db09029aa35fbaaa20a6ba8d3e6ae8d28
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-12-13 13:28:25 -0600

    Update farmersmarketrepository.java

--- src/main/java/com/marketfinder/farmersmarketrepository.java
+++ src/main/java/com/marketfinder/farmersmarketrepository.java
@@ -1,4 +1,13 @@
+package com.marketfinder.repository;
+
+import com.marketfinder.model.FarmersMarket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
 public interface FarmersMarketRepository extends JpaRepository<FarmersMarket, Long> {
     List<FarmersMarket> findByDayOfWeek(String dayOfWeek);
     List<FarmersMarket> findByVendorTypesContaining(String vendorType);
+    List<FarmersMarket> findByLocationContaining(String location);
 }
