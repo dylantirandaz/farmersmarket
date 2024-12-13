48a8e4e179308a14c343a16a007e7adb850ee1ce
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-12-13 13:30:27 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/tailwind.css'
 
-createApp(App).mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
+
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import MarketList from '@/views/MarketList.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: MarketList
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
